3ef1c3c4ce1c8ae3ba3a2394e0ea085b3aa3ec8c
KushiiSubwaySlammer <hayden.manliguez@gmail.com>
2025-11-18 22:40:18 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 function freedomizer(){
   location.href = "./freedomizer.html/main.html";
 }
-
+function IRS(){
+  location.href = "./irssimulator.html/main.html";
+}
+function Numbers(){
+  location.href= "./numbers2electricboogaloo.html/main.html";
+}
+function Mafia(){
+  location.href= "./Thedistributionofthemons.html/main.html";
+}
